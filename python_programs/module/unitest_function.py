80d9f443620ad077d72812aac999b699bb178fa5
kwabena sapong <kwabenasapong@gmail.com>
2022-10-21 10:23:52 +0000

    unit testing

---
+++ python_programs/module/unitest_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# function to do simple calculations
+
+def add(a, b):
+    return (a + b)
+
+def sub(a, b):
+    return (a - b)
+
+def mul(a, b):
+    return (a * b)
+
+def div(a, b):
+    return (a / b)
+
+def mod(a, b):
+    return (a % b)
