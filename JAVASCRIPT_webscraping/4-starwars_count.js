e99e8810ca14e9742e7422889801d25875530671
kolaoba <kolaobajuluwa@gmail.com>
2023-01-17 13:01:20 +0100

    added tasks 2 to 6

--- JAVASCRIPT_webscraping/4-starwars_count.js
+++ JAVASCRIPT_webscraping/4-starwars_count.js
@@ -5,16 +5,33 @@ const url = process.argv[2];
 
 req.get(url,
   function (err, res, body) {
-	  jsonBody = JSON.parse(body);
-	  results = jsonBody.results;
-	  let count = 0;
-	  results.forEach(function (movie, idx) {
-		  characters = movie.characters;
-		  charachers.forEach(function (character, idx) {
-			  if (character.includes('18')) {
-				  count++;
-			  }
-		  });
-	  });
-	  console.log(count);
+    jsonBody = JSON.parse(body);
+    results = jsonBody.results;
+    let count = 0;
+    results.forEach(function (movie, idx) {
+      characters = movie.characters;
+      characters.forEach(function (character, idx) {
+        if (character.includes('18')) {
+          count++;
+        }
+      });
+    });
+    console.log(count);
   });
+
+/*  for (let i = 0; i++; i < results.length) {
+  		characters = results[i].characters;
+		for (let j = 0; j++; j < characters.length)) {
+				if (characters[j].includes('18')) {
+					count ++;
+				}
+		}
+	}
+*/
+
+
+
+
+
+
+
