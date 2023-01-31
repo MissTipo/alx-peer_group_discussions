038e8e3d68e89cad607b8516e1001d318ce3fffe
kolaoba <kolaobajuluwa@gmail.com>
2023-01-31 11:35:30 +0100

    add states and app.py to AirBnB_clone_v4

--- AirBnb_clone/AirBnB_clone_v2/api/v1/app.py
+++ AirBnb_clone/AirBnB_clone_v2/api/v1/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 
 from flask import Flask, jsonify
 from models import storage
--- AirBnb_clone/AirBnB_clone_v2/api/v1/views/states.py
+++ AirBnb_clone/AirBnB_clone_v2/api/v1/views/states.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
-@app_views.route('/states', strict_slashes=False)
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def list_states():
     states = storage.all(State)
     return jsonify(
         [state.to_dict() for state in states.values()]
             )
+
+@app_views.route('/states',methods=['POST'], strict_slashes=False)
+def create_state():
+    data = request.get_json()
+    if data is None:
+        abort(404, "Not a JSON")
+    if data.get("name") is None:
+        abort(404, "Missing Name")
+    new_state = State(**data)
+    print(new_state)
+    return jsonify(new_state.to_dict().get("name"))
 
 @app_views.route('/states/<state_id>', strict_slashes=False)
 def get_state(state_id):
